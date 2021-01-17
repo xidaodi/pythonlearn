b442d5321e52ecdcbfc974d66b8be6f9eca631b2
xidaodi <35069982+xidaodi@users.noreply.github.com>
2021-01-17 20:23:07 +0800

    2021-01-17 20:22 pm

---
+++ day7_1601_1901_几种类型的文件解析&Django.py
@@ -0,0 +1,11 @@
+'''
+
+Chap.16.1-16.5 CSV文件操作说明
+Chap.16.7-16.10 Excel文件操作说明
+Chap.17 XML文件操作说明
+Chap.18 json文件操作说明
+Chap.19 Django开发参考教程：https://docs.djangoproject.com/en/3.1/
+
+以上代码实践部分可以参考github pythonroad repo的源码
+
+'''
